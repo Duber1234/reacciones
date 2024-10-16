54057fdbfbb01cb34d4f2cb8d07d2551cf32a8a2
Duber1234 <dfpesca@misena.edu.co>
2024-10-16 08:29:40 -0500

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -426,7 +426,8 @@
                                     in their infancy.
                                 </p>
                                 <div class="plyr__video-embed plyr-youtube">
-                                    <iframe src="https://www.youtube.com/embed/WeA7edXsU40" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <iframe src="https://www.youtube.com/embed/ugzB87QzIK0?si=BlpACYWzcyVcR2cx" frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen ></iframe>
+                              
                                 </div>
                                 <div class="post-meta">
                                     <button class="post-meta-like">
