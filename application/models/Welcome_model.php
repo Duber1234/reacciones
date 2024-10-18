4e8cab49cd9517577b0e28548de44aab8d761d79
Duber1234 <dfpesca@misena.edu.co>
2024-10-18 01:02:10 -0500

    1

--- application/models/Welcome_model.php
+++ application/models/Welcome_model.php
@@ -84,7 +84,7 @@ if ($dias > 0) {
     $texto= "Hace $dias días con $horas horas y $minutos minutos.";
 } elseif ($horas > 0) {
     // Si han pasado más de 1 hora pero menos de 1 día
-    $texto= "Hace $horas horas con $minutos minutos.";
+    $texto= "Ahora";
 } else {
     // Si han pasado menos de 1 hora
     $texto= "Hace $minutos minutos.";
