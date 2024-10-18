61aca72ae273754882fd2b11c17ab8246e6e69fb
Duber1234 <dfpesca@misena.edu.co>
2024-10-18 00:24:08 -0500

    Update Welcome_model.php

--- application/models/Welcome_model.php
+++ application/models/Welcome_model.php
@@ -60,7 +60,7 @@ class Welcome_model extends CI_Model
     }
     public function get_lista_publicaciones(){
         
-      return $this->db->query("select * from publicacion order by id asc")->result_array();
+      return $this->db->query("select * from publicacion order by id desc")->result_array();
     }
     public function diferencia_m($fecha){
         
