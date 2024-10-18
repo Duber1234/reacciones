a36f2773c68f9ac7b7a3c18c38ae5bd2acb23b9c
Duber1234 <dfpesca@misena.edu.co>
2024-10-18 07:00:23 -0500

    1

--- application/views/base/header.php
+++ application/views/base/header.php
@@ -11,50 +11,51 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="<?=base_url()?>assets/images/favicon2.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon2.png">
 
     <!-- CSS
 	============================================ -->
-    <!-- google fonts -->
-
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900" rel="stylesheet">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?=FCPATH  ?>assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
     <!-- Icon Font CSS -->
-    <link rel="stylesheet" href="<?=FCPATH  ?>assets/css/vendor/bicon.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/bicon.min.css">
     <!-- Flat Icon CSS -->
-    <link rel="stylesheet" href="<?=FCPATH  ?>assets/css/vendor/flaticon.css">
+    <link rel="stylesheet" href="assets/css/vendor/flaticon.css">
     <!-- audio & video player CSS -->
-    <link rel="stylesheet" href="<?=base_url('assets/css/plugins/plyr.css')?>">
+    <link rel="stylesheet" href="assets/css/plugins/plyr.css">
     <!-- Slick CSS -->
-    <link rel="stylesheet" href="<?=base_url('assets/css/plugins/slick.min.css')?>">
+    <link rel="stylesheet" href="assets/css/plugins/slick.min.css">
     <!-- nice-select CSS -->
-    <link rel="stylesheet" href="<?=base_url()?>assets/css/plugins/nice-select.css">
+    <link rel="stylesheet" href="assets/css/plugins/nice-select.css">
     <!-- perfect scrollbar css -->
-    <link rel="stylesheet" href="<?=base_url()?>assets/css/plugins/perfect-scrollbar.css">
+    <link rel="stylesheet" href="assets/css/plugins/perfect-scrollbar.css">
     <!-- light gallery css -->
-    <link rel="stylesheet" href="<?=base_url('assets/css/plugins/lightgallery.min.css')?>">
+    <link rel="stylesheet" href="assets/css/plugins/lightgallery.min.css">
     <!-- Main Style CSS -->
-    <link rel="stylesheet" href="<?=base_url('assets/css/style.css')?>?v=<?php echo time(); ?>">
-     <script src="<?=base_url()?>assets/js/vendor/modernizr-3.6.0.min.js"></script>
+     <link rel="stylesheet" href="<?=base_url('assets/css/style.css')?>?v=<?php echo time(); ?>">
+    <!-- Main Style CSS -->
+   
+<!-- Modernizer JS -->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
     <!-- jQuery JS -->
-    <script src="<?=base_url()?>assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
     <!-- Bootstrap JS -->
-    <script src="<?=base_url()?>assets/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
     <!-- Slick Slider JS -->
-    <script src="<?=base_url()?>assets/js/plugins/slick.min.js"></script>
+    <script src="assets/js/plugins/slick.min.js"></script>
     <!-- nice select JS -->
-    <script src="<?=base_url()?>assets/js/plugins/nice-select.min.js"></script>
+    <script src="assets/js/plugins/nice-select.min.js"></script>
     <!-- audio video player JS -->
-    <script src="<?=base_url()?>assets/js/plugins/plyr.min.js"></script>
+    <script src="assets/js/plugins/plyr.min.js"></script>
     <!-- perfect scrollbar js -->
-    <script src="<?=base_url()?>assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
     <!-- light gallery js -->
-    <script src="<?=base_url()?>assets/js/plugins/lightgallery-all.min.js"></script>
+    <script src="assets/js/plugins/lightgallery-all.min.js"></script>
     <!-- image loaded js -->
-    <script src="<?=base_url()?>assets/js/plugins/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/plugins/imagesloaded.pkgd.min.js"></script>
     <!-- isotope filter js -->
-    <script src="<?=base_url()?>assets/js/plugins/isotope.pkgd.min.js"></script>
+    <script src="assets/js/plugins/isotope.pkgd.min.js"></script>
     <!-- Main JS -->
     <script src="<?=base_url()?>assets/js/main.js?v=<?php echo time(); ?>"></script>
 </head>
