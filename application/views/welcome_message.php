82c6805a600a91f668774db4f9d0a74de58970f8
Duber1234 <dfpesca@misena.edu.co>
2024-10-18 00:34:20 -0500

    1

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -296,9 +296,7 @@
                                     <iframe src="<?=$pl['texto'] ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  allowfullscreen ></iframe>
                               
                                 </div>
-                                <div class="plyr__video-embed plyr-video">
-                                    <iframe src="https://www.youtube.com/embed/odUtqDz4lEk" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                </div>
+                              
                                 <div class="post-meta">
                                     <button class="post-meta-like">
                                         <i class="bi bi-heart-beat"></i>
