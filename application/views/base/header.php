045139b300122ac173a839f32f2ebc962cc98684
Duber1234 <dfpesca@misena.edu.co>
2024-10-18 06:49:13 -0500

    1

--- application/views/base/header.php
+++ application/views/base/header.php
@@ -19,11 +19,11 @@
 
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900" rel="stylesheet">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?=base_url('assets/css/vendor/bootstrap.min.css')?>">
+    <link rel="stylesheet" href="<?=FCPATH  ?>assets/css/vendor/bootstrap.min.css">
     <!-- Icon Font CSS -->
     <link rel="stylesheet" href="<?=FCPATH  ?>assets/css/vendor/bicon.min.css">
     <!-- Flat Icon CSS -->
-    <link rel="stylesheet" href="<?=FCPATH  ?>'assets/css/vendor/flaticon.css'">
+    <link rel="stylesheet" href="<?=FCPATH  ?>assets/css/vendor/flaticon.css">
     <!-- audio & video player CSS -->
     <link rel="stylesheet" href="<?=base_url('assets/css/plugins/plyr.css')?>">
     <!-- Slick CSS -->
