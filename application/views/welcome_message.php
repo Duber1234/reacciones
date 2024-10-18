436da3972f816c507a87b7c2839c0ebd562194f1
Duber1234 <dfpesca@misena.edu.co>
2024-10-18 00:05:06 -0500

    1

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -254,7 +254,7 @@
                         </div>
                         <!-- share box end -->
  <!-- inicio publicaciones -->
- 
+
  <?php foreach ($lista_p as $key => $pl) {?>
  <div class="card">
                             <!-- post title start -->
@@ -271,7 +271,7 @@
 
                                 <div class="posted-author">
                                     <h6 class="author"><a href="profile.html">william henry</a></h6>
-                                    <span class="post-time"><?=$this->welcome->diferencia_m($pl->fecha);  ?> min ago</span>
+                                    <span class="post-time"><?=$this->welcome->diferencia_m($pl->fecha);  ?></span>
                                 </div>
 
                                 <div class="post-settings-bar">
