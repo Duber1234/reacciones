b94804a8b2dcf3a979b215b0b57769d629e9f182
Duber1234 <dfpesca@misena.edu.co>
2024-10-18 05:52:36 -0500

    1

--- application/views/base/header.php
+++ application/views/base/header.php
@@ -35,23 +35,23 @@
     <link rel="stylesheet" href="<?=base_url()?>assets/css/plugins/lightgallery.min.css">
     <!-- Main Style CSS -->
     <link rel="stylesheet" href="<?=base_url()?>assets/css/style.css?v=<?php echo time(); ?>">
-     <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+     <script src="<?=base_url()?>assets/js/vendor/modernizr-3.6.0.min.js"></script>
     <!-- jQuery JS -->
-    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="<?=base_url()?>assets/js/vendor/jquery-3.6.0.min.js"></script>
     <!-- Bootstrap JS -->
-    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="<?=base_url()?>assets/js/vendor/bootstrap.bundle.min.js"></script>
     <!-- Slick Slider JS -->
-    <script src="assets/js/plugins/slick.min.js"></script>
+    <script src="<?=base_url()?>assets/js/plugins/slick.min.js"></script>
     <!-- nice select JS -->
-    <script src="assets/js/plugins/nice-select.min.js"></script>
+    <script src="<?=base_url()?>assets/js/plugins/nice-select.min.js"></script>
     <!-- audio video player JS -->
-    <script src="assets/js/plugins/plyr.min.js"></script>
+    <script src="<?=base_url()?>assets/js/plugins/plyr.min.js"></script>
     <!-- perfect scrollbar js -->
-    <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="<?=base_url()?>assets/js/plugins/perfect-scrollbar.min.js"></script>
     <!-- light gallery js -->
-    <script src="assets/js/plugins/lightgallery-all.min.js"></script>
+    <script src="<?=base_url()?>assets/js/plugins/lightgallery-all.min.js"></script>
     <!-- image loaded js -->
-    <script src="assets/js/plugins/imagesloaded.pkgd.min.js"></script>
+    <script src="<?=base_url()?>assets/js/plugins/imagesloaded.pkgd.min.js"></script>
     <!-- isotope filter js -->
     <script src="assets/js/plugins/isotope.pkgd.min.js"></script>
     <!-- Main JS -->
