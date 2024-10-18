3ab123f4e0e93d471755abc766e0ebe0a643bef9
Duber1234 <dfpesca@misena.edu.co>
2024-10-18 00:26:47 -0500

    1

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -293,7 +293,7 @@
                                     
                                 
                                 <div class="plyr__video-embed plyr-youtube">
-                                    <iframe src="https://www.youtube.com/embed/ugzB87QzIK0?si=BlpACYWzcyVcR2cx" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen ></iframe>
+                                    <iframe src="<?=$pl['texto'] ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen ></iframe>
                               
                                 </div>
                                 <div class="post-meta">
@@ -494,7 +494,7 @@
                                     in their infancy.
                                 </p>
                                 <div class="plyr__video-embed plyr-youtube">
-                                    <iframe src="https://www.youtube.com/embed/ugzB87QzIK0?si=BlpACYWzcyVcR2cx" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen ></iframe>
+                                    <iframe src="https://www.youtube.com/watch?v=hqMp1FZnYlg&list=PLyrPDNECcgnpcPIabXBpuGZVaOS67PLHW&ab_channel=DuberPesca" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen ></iframe>
                               
                                 </div>
                                 <div class="post-meta">
