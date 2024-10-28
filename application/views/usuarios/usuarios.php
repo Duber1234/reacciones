9c2a946fca3f2ebd43a53783acb011f8c7b9c3ff
Duber1234 <dfpesca@misena.edu.co>
2024-10-28 08:35:40 -0500

    Update usuarios.php

--- application/views/usuarios/usuarios.php
+++ application/views/usuarios/usuarios.php
@@ -264,7 +264,7 @@
                         <!-- share box end -->
  <!-- inicio publicaciones -->
 <div class="plyr__video-embed plyr-video">
- <video controls autoplay loop muted width="100%" height="240">
+ <video controls autoplay width="100%" height="240">
                                   <source src="<?=base_url() ?>/assets/PELICULAS/1.mp4" type="video/mp4">
                                         Your browser does not support the video tag.
                                     </video>
